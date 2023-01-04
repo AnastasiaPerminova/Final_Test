0651b861f44820d802bbe473a455ec62b6648f18
Anastasia Perminova <tasitenok1987@yandex.ru>
2023-01-04 22:47:58 +0300

    Added ask program

--- Program.cs
+++ Program.cs
@@ -6,3 +6,35 @@
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
 
+Console.WriteLine("Введите  количество элементов в массиве : ");
+int number = Convert.ToInt32(Console.ReadLine ());
+string[] array = new string [number];
+int count =0;
+for (int i = 0; i < array.Length; i++)
+{   
+    Console.WriteLine($"Введите {i + 1}-й элемент  в массив : ");
+    string element =Console.ReadLine ();
+    array [i] = element;
+    if (element.Length <= 3)
+    {
+        count++;
+    }
+
+}
+Console.WriteLine("Были введены строки: ");
+Console.WriteLine('[' + string.Join(", ", array) + ']');
+string[] array2 =  new string [count];
+int count2 = 0;
+for (int i = 0; i < array.Length; i++)
+{   
+  
+    if (array [i].Length <= 3)
+    {
+        array2[count2] = array[i];
+        count2++;
+    }
+
+}
+
+Console.WriteLine("Строки длина которых меньше, либо равна 3 символам: ");
+Console.WriteLine('[' + string.Join(", ", array2) + ']');
